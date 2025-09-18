1636bd8027f0d99575eaf06adbe157de7043d2f2
Shoxruh <shnarzullayevich@gmail.com>
2025-09-17 21:12:30 -0700

    fixed error

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -4,14 +4,9 @@ import * as React from "react"
 import { Command as CommandPrimitive } from "cmdk"
 import { SearchIcon } from "lucide-react"
 
-import { cn } from "@/lib/utils"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog"
+import { cn } from "../../lib/utils"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./dialog"
+
 
 function Command({
   className,
